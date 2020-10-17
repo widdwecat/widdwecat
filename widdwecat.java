8359bb69e78f763ff0cd1c81dd2196d5cf666de8
widdwecat <68664271+widdwecat@users.noreply.github.com>
2020-10-16 20:15:59 -0400

    Create widdwecat.java

---
+++ widdwecat.java
@@ -0,0 +1,8 @@
+package widdwecat;
+
+public class widdwecat {
+    public static void main(String[] args) {
+       public static String widdwecat = "Hey I'm widdwecat!";
+       System.out.println(widdwecat);
+    }
+}
